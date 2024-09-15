5bccf48c390737193ee50642c50ef6a8adb43f79
gabriel cabral <gabrielcabralbastos@gmail.com>
2024-09-14 23:02:00 -0300

    2° commit - Código quase pronto, ele funciona :>

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int num1 = 1, num2 = 0;
+        int fibo = 0;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.print(fibo + " "); //Primeiro termo ( 0 )
+
+        // Dessa forma ele vai imprimir até 377. Caso o numéro passe de 377 ele imprime 610.
+        while (fibo < 377) {
+            fibo = num1 + num2;
+            System.out.print(fibo + " ");
+            num1 = num2;
+            num2 = fibo;
+
         }
     }
+
 }
