cdbfb876af335feff37ed188db63edf5f5c94a04
Gabriel Cabral <gabrielcabralbastos@gmail.com>
2025-02-11 00:51:18 -0300

    11/02/2025 - Modificações estéticas

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
 
 public class Main {
     public static void main(String[] args) {
-        int num1 = 1, num2 = 0;
-        int fibo = 0;
-
-        System.out.print(fibo + " "); //Primeiro termo ( 0 )
+        long num1 = 1, num2 = 0;
+        long fibo = 0;
+        int i = 0;
 
         // Dessa forma ele vai imprimir até 377. Caso o numéro passe de 377 ele imprime 610.
-        while (fibo < 377) {
+        while (fibo < 337) {
+
             fibo = num1 + num2;
-            System.out.print(fibo + " ");
+
+            System.out.println(i+"° número da sequência de Fibonacci: " + fibo);
+
             num1 = num2;
             num2 = fibo;
+            i++;
 
         }
     }
